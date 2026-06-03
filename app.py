bc6746c67f6c515ca7c6e9ff2e33601d9b55a304
mashitara <tnknora@gmail.com>
2026-06-03 22:18:42 +0900

    Flask and MySQL connection

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
+task = ["乗り越えて見せる", "世俗も", "肉欲も"]
+
+conn = mysql.connector.connect(
+    host = "localhost",
+    user = "ms",
+    password = "",
+    database = "todo_app"
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT title FROM tasks")
+
+tasks = cursor.fetchall()
+
+conn.close()
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    
+    return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    #app.run(debug=True)
