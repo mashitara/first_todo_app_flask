cfc49aab62ecf812a0be3904e67f7ef7b4a7aeaa
mashitara <tnknora@gmail.com>
2026-06-04 22:15:59 +0900

    Implement task creation with Flas and MySQL

--- app.py
+++ app.py
@@ -1,28 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import mysql.connector
 
 app = Flask(__name__)
 task = ["乗り越えて見せる", "世俗も", "肉欲も"]
 
-conn = mysql.connector.connect(
-    host = "localhost",
-    user = "ms",
-    password = "",
-    database = "todo_app"
-)
-
-cursor = conn.cursor()
-cursor.execute("SELECT title FROM tasks")
-
-tasks = cursor.fetchall()
-
-conn.close()
-
-
 @app.route("/")
 def index():
-    
+    conn = mysql.connector.connect(
+        host = "localhost",
+        user = "ms",
+        password = "",
+        database = "todo_app"
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tasks")
+
+    tasks = cursor.fetchall()
+
+    conn.close()
     return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add():
+
+    conn = mysql.connector.connect(
+        host = "localhost",
+        user = "ms",
+        password = "",
+        database = "todo_app"
+    )
+
+    cursor = conn.cursor()
+
+    title = request.form["title"]
+    cursor.execute("INSERT INTO tasks (title) VALUES (%s)", (title,))
+
+    conn.commit()
+    conn.close()
+    
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
