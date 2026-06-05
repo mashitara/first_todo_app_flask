cf77c1310e45fe56f2a0d3fc9ffee6c2bddf48fa
mashitara <tnknora@gmail.com>
2026-06-05 23:58:06 +0900

    Complete CRUD functionality

--- app.py
+++ app.py
@@ -41,6 +41,57 @@ def add():
     
     return redirect("/")
 
+@app.route("/delete/<int:task_id>", methods=["POST"])
+def delete(task_id):
+    conn = mysql.connector.connect(
+        host = "localhost",
+        user = "ms",
+        password = "",
+        database = "todo_app"
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM tasks WHERE id = (%s)", (task_id,))
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
+
+@app.route("/edit/<int:task_id>")
+def edit(task_id):
+    conn = mysql.connector.connect(
+        host = "localhost",
+        user = "ms",
+        password = "",
+        db = "todo_app"
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tasks WHERE id = (%s) ", (task_id,))
+    task = cursor.fetchone()
+
+    return render_template("edit.html", task=task)
+
+@app.route("/update/<int:task_id>", methods=["POST"])
+def update(task_id):
+    conn = mysql.connector.connect(
+        host = "localhost",
+        user = "ms",
+        password = "",
+        db = "todo_app"
+    )
+
+    title = request.form["title"]
+    cursor = conn.cursor()
+    cursor.execute("UPDATE tasks SET title = (%s) WHERE id = (%s) ", (title, task_id,))
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
     #app.run(debug=True)
