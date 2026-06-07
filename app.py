856e35fc9284f9c35c996f85a2301eb444c8eb57
mashitara <tnknora@gmail.com>
2026-06-07 22:23:02 +0900

    並び替えを実装しました

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
@@ -12,9 +12,15 @@ def index():
         password = "",
         database = "todo_app"
     )
+    sort = request.args.get("sort", "new")
+
+    if sort == "new":
+        sql = "SELECT * FROM tasks ORDER BY id DESC"
+    else:
+        sql = "SELECT * FROM tasks ORDER BY id ASC"
 
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM tasks")
+    cursor.execute(sql)
 
     tasks = cursor.fetchall()
 
@@ -101,12 +107,14 @@ def toggle(task_id):
         db = "todo_app"
     )
 
+    sort = request.args.get("sort", "new")
+
     cursor = conn.cursor()
     cursor.execute("UPDATE tasks SET completed = NOT completed WHERE id = (%s)", (task_id,))
     conn.commit()
     conn.close()
 
-    return redirect("/")
+    return redirect(url_for("index", sort=sort))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
